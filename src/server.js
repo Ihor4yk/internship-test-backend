c8dec0a602cc89ee423da9edb2eadb52a3cc22e3
Ihor4yk <igor4yk.lyashen4yk@gmail.com>
2026-02-24 15:11:30 +0200

    feat: add /api prefix to all backend routes

--- src/server.js
+++ src/server.js
@@ -22,14 +22,14 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-app.use(rootRoutes);
-app.use(authRoutes);
-app.use(applicationRoutes);
-app.use(dealRoutes);
+app.use("/api", rootRoutes);
+app.use("/api", authRoutes);
+app.use("/api", applicationRoutes);
+app.use("/api", dealRoutes);
 
-app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api/swagger", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.use(healthRoutes);
+app.use("/api", healthRoutes);
 
 app.use(errors());
 app.use(notFoundHandler);
