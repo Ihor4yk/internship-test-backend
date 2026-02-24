d748278e055ca01df2214f8bbe52f4fe9c24d1a9
Ihor4yk <igor4yk.lyashen4yk@gmail.com>
2026-02-24 20:21:33 +0200

    update getDeals

--- src/controllers/dealController.js
+++ src/controllers/dealController.js
@@ -3,7 +3,9 @@ import { pool } from "../db/connectPostgreSQL.js";
 import createHttpError from "http-errors";
 
 export const getDeals = asyncHandler(async (req, res, next) => {
-  const result = await pool.query("SELECT * FROM deals ORDER BY id");
+  const result = await pool.query(
+    "SELECT * FROM deals ORDER BY created_at DESC",
+  );
 
   if (!result.rows.length) {
     next(createHttpError(404, "No deals found"));
