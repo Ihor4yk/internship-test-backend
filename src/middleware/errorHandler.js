a69026093e62dffcef8953b3251b954040df6fde
Ihor4yk <igor4yk.lyashen4yk@gmail.com>
2026-02-24 19:06:40 +0200

    fix: update global error handler logic

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,13 +1,23 @@
 export const errorHandler = (err, req, res, next) => {
+  const status = err.status || err.statusCode || 500;
   const isProd = process.env.NODE_ENV === "production";
 
-  const status = err.status || err.statusCode || 500;
+  // логування серверних помилок
+  if (status >= 500) {
+    console.error(err);
+  }
 
+  // очікувані помилки
+  if (status < 500) {
+    return res.status(status).json({
+      message: err.message,
+    });
+  }
+
+  // серверна помилка
   res.status(status).json({
     message: isProd
       ? "Something went wrong. Please try again later."
       : err.message,
-    ...(isProd && { error: "Check server logs for details" }),
   });
 };
-
